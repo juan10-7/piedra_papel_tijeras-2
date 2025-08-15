f1554c06bb73dee5bef682c22b27ce38f760d602
juan10-7 <juansantan1@colegioguanenta.edu.co>
2025-08-15 07:14:57 -0500

    se actualizo

--- img/codigo.py
+++ img/codigo.py
@@ -1,61 +1,97 @@
-from tkinter import *
-from tkinter import messagebox
+from tkinter import*
 import random
+from tkinter import messagebox
 
-mag = random.randint(1,3)
+# cuando el usuario elige piedra 
+def piedra_usuario():
+    messagebox.showinfo("","opcion piedra")
+    # opcion de la maquina
+    maq = random.randint(1,3)
+    if maq == 1: 
+        resultado = "empate, la maquina eligio piedra"
+    elif maq == 2:
+        resultado = "perdiste, la maquina eligio papel"
+    else:
+        resultado = "ganaste, la maquina eligio tijera"
+    t_resultados.insert(INSERT, resultado + "\n")
+# cuando el usuario elige papel
+def papel_usuario():
+    messagebox.showinfo("","opcion papel")
+    # opcion de la maquina
+    maq = random.randint(1,3)
+    if maq == 1: 
+        resultado = "ganaste, la maquina eligio piedra"
+    elif maq == 2:
+        resultado = "empate, la maquina eligio papel"
+    else:
+        resultado = "perdiste, la maquina eligio tijera"
+    t_resultados.insert(INSERT, resultado + "\n")
 
-ventana_principal = Tk()
+# cuando el usuario elige tijera
+def tijera_usuario():
+    messagebox.showinfo("","opcion tijera")
+    # opcion de la maquina
+    maq = random.randint(1,3)
+    if maq == 1: 
+        resultado = "perdiste, la maquina eligio piedra"
+    elif maq == 2:
+        resultado = "ganaste, la maquina eligio papel"
+    else:
+        resultado = "empate, la maquina eligio tijera"
+    t_resultados.insert(INSERT, resultado + "\n")
 
-# Titulo de la ventana
-ventana_principal.title("Juan fernando satana cala")
+ventana = Tk()
+ventana.title("juan fernando santana cala")
+ventana.geometry("900x500")
+ventana.resizable(0,0)
+ventana.config(bg="black")
 
-# Tamaño de la ventana
-ventana_principal.geometry("800x500")  
+frame_1= Frame(ventana)
+frame_1.config(bg="red", width=880,height=240)
+frame_1.place(x=10,y=10)
 
-# Deshabilitar boton maximizar de la ventana
-ventana_principal.resizable(0,0)
+# etiqueta para el titulode la app
+titulo = Label(frame_1,text=" Colegio San José de Guanenta")
+titulo.config(bg="red", fg="black",font=("arial",20))
+titulo.place(x=220,y=10)
+# Etiqueta para subtitulo 1 de la app
+subtitulo1 = Label(frame_1,text="  Especialidad en sistemas")
+subtitulo1.config(bg="red",fg="black",font=("arial",18))
+subtitulo1.place(x=250,y=45)
+#Etiqueta subtitulo2
+subtitulo2 = Label(frame_1,text="PIEDRA PAPEL O TIJERA")
+subtitulo2.config(bg="red",fg="black",font=("arial",30),anchor=CENTER)
+subtitulo2.place(x=180,y=120)
 
-# Color de fondo de la pantalla
-ventana_principal.config(bg="blue")
-
-frame_1 = Frame(ventana_principal)
-frame_1.config(bg="ivory", width=780, height=240)
-frame_1.place(x=10, y=10)
-
-titulo = Label(frame_1, text="piedra_papel_tijera")
-titulo.config(bg="white", fg="black", font=("Arial", 16))
-titulo.place(x=300,y=10)
-
-titulo = Label(frame_1, text="opciones del jugador")
-titulo.config(bg="yellow", fg="black", font=("Arial", 16))
-titulo.place(x=300,y=70)
-
-frame_2 = Frame(ventana_principal)
-frame_2.config(bg="ivory2", width=780, height=120)
-frame_2.place(x=10, y=260)
-
-img_bt_papel = PhotoImage(file="img/papel.png")
-bt_papel =  Button(frame_2, image = img_bt_papel, width=105, height=105)
-#bt_papel = Button(frame_2, text="papel", width=10)
-bt_papel.place(x=337, y=7)
-
-img_bt_tijera = PhotoImage(file="img/tijera.png")
-bt_tijera =  Button(frame_2, image = img_bt_tijera, width=105, height=105)
-#bt_tjera = Button(frame_2, text="papel", width=10)
-bt_tijera.place(x=558, y=7)
-
+#_-------------------------
+# frame 2 - panel de operaciones
+#--------------------------
+frame_2= Frame(ventana)
+frame_2.config(bg="red", width=880,height=120)
+frame_2.place(x=10,y=260)
 img_bt_piedra = PhotoImage(file="img/piedra.png")
-bt_piedra =  Button(frame_2, image = img_bt_piedra, width=105, height=105)
-#bt_piedra = Button(frame_2, text="piedra", width=10)
-bt_piedra.place(x=116, y=7)
+bt_piedra = Button(frame_2, image = img_bt_piedra, width = 105, height = 105, command=piedra_usuario)
+bt_piedra.place(x =100, y = 7)
+img_bt_papel = PhotoImage(file="img/papel.png")
+bt_papel = Button(frame_2, image = img_bt_papel, width = 105, height = 105, command=papel_usuario)
+bt_papel.place(x =337, y = 7)
+img_bt_tijeras = PhotoImage(file="img/tijera.png")
+bt_papel = Button(frame_2, image = img_bt_tijeras, width = 105, height = 105, command=tijera_usuario)
+bt_papel.place(x = 570, y = 7)
 
 
-frame_3= Frame(ventana_principal)
-frame_3.config(bg="ivory2", width=780, height=120)
-frame_3.place(x=10, y=390)
-
-
-ventana_principal.mainloop()
+#--------------------------
+# frame 3 -Resultado
+#--------------------------
+frame_3= Frame(ventana)
+frame_3.config(bg="red", width=880,height=120)
+frame_3.place(x=14,y= 390)
+# area de texto
+t_resultados = Text(frame_3, width=54, height=3)
+t_resultados.config(bg="red", fg="black", font=("Courier", 20))
+t_resultados.pack()
+#Metodo principal quedespiega la ventana en pantalla
+ventana.mainloop()
 
 
 
